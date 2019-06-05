250712737a1af87424afed1172b7f61b560e802d
Gael MOTTE <gael.motte@gmail.com>
2019-06-05 12:04:44 +0200

    Adding basic Appex Controler and callout

--- force-app/main/default/lwc/heyGeneralInfo/heyGeneralInfo.js
+++ force-app/main/default/lwc/heyGeneralInfo/heyGeneralInfo.js
@@ -1,10 +1,28 @@
-import { LightningElement, track} from 'lwc';
+import { LightningElement, track, wire} from 'lwc';
+import  basicMethod  from '@salesforce/apex/HeyDefaultController.basicMethod';
+import  callServerTest  from '@salesforce/apex/HeyDefaultController.callServerTest';
 
 export default class HeyGeneralInfo extends LightningElement {
 
-    @track greeting = 'World';
+    @track 
+    greeting = 'World';
+
+    
+
     changeHandler(event) {
         this.greeting = event.target.value;
+        basicMethod().then(result => {
+            this.greeting += result;
+        }).catch(error => {
+            console.log("Something went wrong", error);
+        })
+        
+    }
+
+    buttonClickHandler(event){
+        callServerTest().then(result => {
+            console.log(result);
+        })
     }
 
 }
